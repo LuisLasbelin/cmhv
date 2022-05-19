669c71f27929a377e9efd8f6b698adf05df90557
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-05-19 13:32:58 +0200

    initial state

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const CMHV = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ CMHV.abilities = {
+  "str": "CMHV.AbilityStr",
+  "dex": "CMHV.AbilityDex",
+  "con": "CMHV.AbilityCon",
+  "int": "CMHV.AbilityInt",
+  "wis": "CMHV.AbilityWis",
+  "cha": "CMHV.AbilityCha"
+};
+
+CMHV.abilityAbbreviations = {
+  "str": "CMHV.AbilityStrAbbr",
+  "dex": "CMHV.AbilityDexAbbr",
+  "con": "CMHV.AbilityConAbbr",
+  "int": "CMHV.AbilityIntAbbr",
+  "wis": "CMHV.AbilityWisAbbr",
+  "cha": "CMHV.AbilityChaAbbr"
+};
